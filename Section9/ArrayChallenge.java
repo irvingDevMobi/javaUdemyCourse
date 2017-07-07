54f74957e68bfb08617d74f70a0a32bc4419c9e8
Irving Lop <irving.lopez@globant.com>
2017-07-07 17:54:17 -0500

    first commit to init structure of the repo

---
+++ Section9/ArrayChallenge.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayChallenge {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	static int[] getIntegers(int nInputs) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Type " + nInputs + "numbers");
+		int[] inputs = new int[nInputs];
+		for (int i = 0; i < nInputs; i++) {
+		}
+		return inputs;
+	}
+}
